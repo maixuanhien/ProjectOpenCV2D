f8ef561d29f00095eacc4e7f7245e38ffbfe8774
Unknown <maixuanhien.vn93@gmail.com>
2018-01-16 17:59:16 +0100

    Start

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.IO;
+using System.Drawing;
+using Emgu.CV.Util;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+    private VideoCapture webcam;
+    private Mat webcamFrame;
+    private Mat webcamFrameGray;
+
+    private CascadeClassifier cascadeClassifier;
+    private string path = "D:\\Gamagora\\Interface\\ProjectOpenCV2D\\data\\lbpcascades\\lbpcascade_frontalface_improved.xml";
+
+    private Rectangle[] faces;
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpHeight;
+
+    Rigidbody2D playerBody;
+    Animator playerAnim;
+
+    private int horizontal;
+    private int vertical;
+
+    private bool facingRight;
+    private bool grounded;
+
+    private void Start() {
+        webcam = new VideoCapture(0);
+        webcam.ImageGrabbed += new EventHandler(handleWebcamQueryFrame);
+        webcamFrame = new Mat();
+        webcamFrameGray = new Mat();
+        cascadeClassifier = new CascadeClassifier(path);
+
+        playerBody = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+
+        horizontal = 0;
+        vertical = 0;
+
+        facingRight = true;
+    }
+
+    private void Update() {
+        if (webcam.IsOpened) {
+            webcam.Grab();
+        }
+        if (webcamFrame.IsEmpty) {
+            return;
+        }
+    }
+
+    private void FixedUpdate() {
+        playerBody.velocity = new Vector2(horizontal * speed, playerBody.velocity.y);
+
+        if (horizontal > 0 && !facingRight) {
+            flip();
+        } else if (horizontal < 0 && facingRight) {
+            flip();
+        }
+
+        if (horizontal != 0) {
+            playerAnim.SetBool("run", true);
+        } else {
+            playerAnim.SetBool("run", false);
+        }
+
+        if (vertical > 0) {
+            if (grounded) {
+                grounded = false;
+                playerBody.velocity = new Vector2(playerBody.velocity.x, jumpHeight);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground") {
+            grounded = true;
+        }
+    }
+
+    private void flip() {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x = theScale.x * (-1);
+        transform.localScale = theScale;
+    }
+
+    private void OnDestroy() {
+        webcam.Dispose();
+        CvInvoke.DestroyAllWindows();
+    }
+
+    private void handleWebcamQueryFrame(object sender, EventArgs e) {
+        webcam.Retrieve(webcamFrame);
+
+        Image<Hsv, byte> imageHSV = webcamFrame.ToImage<Hsv, byte>();
+        CvInvoke.CvtColor(imageHSV, imageHSV, ColorConversion.Bgr2Hsv);
+
+        double hValueMin = 70;
+        double hValueMax = 100;
+
+        double sValueMin = 100;
+        double sValueMax = 255;
+
+        double vValueMin = 100;
+        double vValueMax = 255;
+
+        Hsv lower = new Hsv(hValueMin, sValueMin, vValueMin);
+        Hsv upper = new Hsv(hValueMax, sValueMax, vValueMax);
+
+        Mat imgGray = imageHSV.InRange(lower, upper).Mat;
+
+        VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+        VectorOfPoint biggestContour = new VectorOfPoint();
+        int biggestContourIndex = -1;
+        float biggestContourArea = 0f;
+
+        Mat hierarchy = new Mat();
+        CvInvoke.FindContours(imgGray, contours, hierarchy, RetrType.List, ChainApproxMethod.ChainApproxNone);
+
+        for (int i = 0; i < contours.Size; i++) {
+            if (CvInvoke.ContourArea(contours[i]) > biggestContourArea) {
+                biggestContour = contours[i];
+                biggestContourIndex = i;
+                biggestContourArea = (float)CvInvoke.ContourArea(contours[i]);
+            }
+        }
+
+        if (biggestContourIndex > -1) {
+            CvInvoke.DrawContours(webcamFrame, contours, biggestContourIndex, new MCvScalar(255, 0, 0));
+        }
+
+        var moments = CvInvoke.Moments(biggestContour);
+        int cx = (int)(moments.M10 / moments.M00);
+        int cy = (int)(moments.M01 / moments.M00);
+        Point p = new Point(cx, cy);
+        int width = webcamFrame.Width;
+        int height = webcamFrame.Height;
+
+        if (cx < (float)width / 4) {
+            horizontal = 1;
+        } else if (cx > 3 * (float)width / 4) {
+            horizontal = -1;
+        } else {
+            horizontal = 0;
+        }
+        if (cy < (float)height / 4) {
+            vertical = 1;
+        } else if (cy > 3 * (float)height / 4) {
+            vertical = -1;
+        } else {
+            vertical = 0;
+        }
+
+        Mat flippedImage = webcamFrame.Clone();
+        CvInvoke.Flip(webcamFrame, flippedImage, FlipType.Horizontal);
+        CvInvoke.Imshow("Test", flippedImage);
+    }
+}
---
+++ Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    [SerializeField]
+    private float minX = 5;
+    [SerializeField]
+    private float maxX = 90;
+    [SerializeField]
+    private float minY = -5;
+    [SerializeField]
+    private float maxY = 5;
+
+    [SerializeField]
+    public bool isLocked = false;
+
+    float offset;
+
+    void Start() {
+        offset = transform.position.y - target.position.y;
+    }
+
+    private void FixedUpdate() {
+        if (target != null) {
+            if (!isLocked) {
+                Vector3 targetCameraPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+                Vector3 newPos = targetCameraPosition;
+
+                if (targetCameraPosition.x < minX) {
+                    newPos.x = minX;
+                }
+
+                if (targetCameraPosition.x > maxX) {
+                    newPos.x = maxX;
+                }
+
+                if (targetCameraPosition.y < minY) {
+                    newPos.y = minY;
+                }
+
+                if (targetCameraPosition.y > maxY) {
+                    newPos.y = maxY;
+                }
+
+                //transform.position = Vector3.Lerp(transform.position, newPos, smoothing * Time.deltaTime);
+                transform.position = newPos;
+            }
+        }
+        checkIfInsideCameraBox();
+    }
+
+    public void lockCamera() {
+        this.isLocked = true;
+    }
+
+    public void unlockCamera() {
+        this.isLocked = false;
+    }
+
+    //Check if the player is inside the camera box. If not, it changes it position to make it so or kill him if he fall.
+    private void checkIfInsideCameraBox() {
+        Vector3 playerPositionCamera = Camera.main.WorldToScreenPoint(target.position);
+        if (target != null) {
+            if (playerPositionCamera.x < 0) {
+                target.position = Camera.main.ScreenToWorldPoint(new Vector3(0, playerPositionCamera.y, playerPositionCamera.z));
+            } else if (playerPositionCamera.x > Camera.main.pixelWidth) {
+                target.position = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, playerPositionCamera.y, playerPositionCamera.z));
+            }
+            if (playerPositionCamera.y < 0) {
+                //target.GetComponent<PlayerHealth>().makeDead();
+            }
+        }
+    }
+
+}
